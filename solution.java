3b417cf929c74b176d036e2f73039006b58be0bd
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-18 17:14:18 -0500

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+
+        //create variables
+
+        int s = 10;
+
+        //print the variable
+
+        System.out.println("s = "  +  s );
+
+        // floats
+
+        float a = 5;
+        float r = 10;
+        float n = a/r;
+
+        //print output
+        System.out.println(n);
+
+        // modular operator
+        
+
+
+
+
+    }
+}
